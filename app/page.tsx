0a7a1db6f5f7c68ca0bb4c026b1177bc5d81a93b
linux-superuser666 <linux-superuser666@users.noreply.github.com>
2026-02-10 20:41:00 +0800

    create particle and glitch text

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
     <div className="relative gap-8 font-rajdhani w-full h-screen flex flex-col">
       <div className="fixed -z-10 inset-0 bg-black"></div>
       <div className="min-w-[100px] gap-0 font-rajdhani w-full h-full flex flex-col justify-start absolute inset-0 items-center">
-        {/* <GlitchText className="!text-5xl" text="Cyberdeck blackarch" />
+        <GlitchText className="!text-5xl" text="Cyberdeck blackarch" />
         <GlitchText
           className="text-yellowx !text-base font-semibold uppercase"
           text="crafting with passion by © mrnngstr666"
-        ></GlitchText> */}
+        ></GlitchText>
         <Hero />
       </div>
-      {/* <Particles className="absolute inset-0 w-full h-screen -z-10" /> */}
+      <Particles className="absolute inset-0 w-full h-screen -z-10" />
     </div>
   );
 }
