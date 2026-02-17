0655e5d0dc4afca9ce0df8020fb946bad22ada2f
linux-superuser666 <linux-superuser666@users.noreply.github.com>
2026-02-17 19:40:45 +0800

    fix screen size add respon

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       onDoubleClick={handleFullscreen}
       className="font-rajdhani flex mx-auto w-full min-h-screen relative overflow-hidden justify-center items-center"
     >
-      <div className="size-full max-w-[1340px] md:max-h-[630px] max-h-[630px] absolute bottom-0 flex z-10 justify-center items-end">
+      <div className="size-full max-w-[1340px] md:max-h-[630px] max-h-[630px] absolute bottom-0 min-2xl:hidden flex z-10 justify-center items-end">
         <SideWidget />
       </div>
 
